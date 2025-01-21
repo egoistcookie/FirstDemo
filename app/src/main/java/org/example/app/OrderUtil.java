eef22cec3b707a71111195e5609541af4a8472e3
egoistcookie <Srcb@202401>
2025-01-22 00:42:51 +0800

    20250122-lf:first class：only connect github.

---
+++ app/src/main/java/org/example/app/OrderUtil.java
@@ -0,0 +1,11 @@
+package org.example.app;
+
+public class OrderUtil {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, GitHub Java Project!");
+        System.out.println("1");
+    }
+
+}
