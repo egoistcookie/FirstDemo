2ee4905aadbec4d1f29d358b32e024b5d5482f69
EgoistCookie <335985579@qq.com>
2025-03-04 20:43:18 +0800

    lf:字符类的算法类，添加第2种算法：找到字符串中最长的回文子串。

--- src/main/java/com/algorithm/AlgorithmAboutStr.java
+++ src/main/java/com/algorithm/AlgorithmAboutStr.java
@@ -8,10 +8,89 @@ public class AlgorithmAboutStr {
     public static void main(String[] args) {
 
         // 力扣算法3.找一个字符串中最长不重复子串的长度
-        String str1 = "dadv";
-        System.out.println(getMaxChildStrLength(str1));
+        // String str1 = "dadv";
+        // System.out.println(getMaxChildStrLength(str1));
+
+        // 力扣算法5.找到字符串中最长的 回文 子串。（回文：正序逆序都一样）
+        // String str = "cbabad";
+        String str = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        String reStr = getManacher(str);
+        System.out.println(reStr);
+
+    }
+
+    private static String getManacher(String s ){
 
 
+        if (s == null || s.length() == 0) return "";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("^");
+        for (int i = 0; i < s.length(); i++) {
+            sb.append("#").append(s.charAt(i));
+        }
+        sb.append("#$");
+        String T = sb.toString();
+        int n = T.length();
+        int[] p = new int[n];
+        int center = 0, right = 0;
+        int maxLen = 0, maxCenter = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < right) {
+                p[i] = Math.min(right - i, p[mirror]);
+            }
+
+            while (T.charAt(i + p[i] + 1) == T.charAt(i - p[i] - 1)) {
+                p[i]++;
+            }
+
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                maxCenter = i;
+            }
+        }
+
+        int start = (maxCenter - maxLen) / 2;
+        return s.substring(start, start + maxLen);
+
+    }
+
+    /**
+     * 找到字符串中最长的 回文 子串
+     * @param str 原字符
+     * @return 回文字符
+     */
+    private static String getPalindromeStr(String str) {
+        String palinStr = "";
+        int palLength = 0;
+        String reStr = "";
+        for(int i=0;i<str.length();i++){
+
+            char inChr = str.charAt(i);
+            reStr += inChr;
+            String palStr = "";
+            System.out.println(reStr);
+            for(int j=reStr.length() ; j>0; j--){
+                palStr += reStr.charAt(j-1);
+            }
+            System.out.println("颠倒数："+palStr);
+            if(reStr.equals(palStr)){
+                System.out.println("为回文数："+reStr);
+                if(reStr.length() > palLength){
+                    palLength = reStr.length();
+                    palinStr = reStr;
+                }
+            }
+        }
+        return palinStr;
     }
 
     /**
