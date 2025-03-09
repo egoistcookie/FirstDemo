85786c28132cb1484b1cf9859a80bdfcb249ce29
egoistcookie <335985579@qq.com>
2025-03-09 10:57:22 +0800

    lf：AlgorithmAboutCustomStruct：算法208.Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。以自定义树节点类实现。

--- src/main/java/com/algorithm/AlgorithmAboutCustomStruct.java
+++ src/main/java/com/algorithm/AlgorithmAboutCustomStruct.java
@@ -1,8 +1,6 @@
 package com.algorithm;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AlgorithmAboutCustomStruct {
 
@@ -22,19 +20,140 @@ public class AlgorithmAboutCustomStruct {
         // System.out.println(lRUCache.get(3));    // 返回 3
         // System.out.println(lRUCache.get(4));    // 返回 4
         // ds算法：双向链表
-        LRUCacheByDs lRUCache = new LRUCacheByDs(2);
-        lRUCache.put(1, 1); // 缓存是 {1=1}
-        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-        System.out.println(lRUCache.get(1));    // 返回 1
-        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
-        System.out.println(lRUCache.get(2));    // 返回 -1 (未找到)
-        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
-        System.out.println(lRUCache.get(1));    // 返回 -1 (未找到)
-        System.out.println(lRUCache.get(3));    // 返回 3
-        System.out.println(lRUCache.get(4));    // 返回 4
+        // LRUCacheByDs lRUCache = new LRUCacheByDs(2);
+        // lRUCache.put(1, 1); // 缓存是 {1=1}
+        // lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        // System.out.println(lRUCache.get(1));    // 返回 1
+        // lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        // System.out.println(lRUCache.get(2));    // 返回 -1 (未找到)
+        // lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        // System.out.println(lRUCache.get(1));    // 返回 -1 (未找到)
+        // System.out.println(lRUCache.get(3));    // 返回 3
+        // System.out.println(lRUCache.get(4));    // 返回 4
+
+        // 力扣算法208.Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。
+        //  这一数据结构有相当多的应用情景，例如自动补全和拼写检查。
+        Trie trie = new Trie();
+        // trie.insert("apple");
+        System.out.println(trie.search("apple"));   // 返回 True
+        System.out.println(trie.search("app"));     // 返回 False
+        System.out.println(trie.startsWith("app")); // 返回 True
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // 返回 True
 
 
     }
+
+    /**
+     * 树节点
+     */
+    static class TrieNode {
+
+        private TrieNode[] children = new TrieNode[26]; // 假设仅小写字母
+        private boolean isEnd;
+
+        public boolean containsKey(char c) { return children[c - 'a'] != null; }
+        public TrieNode get(char c) { return children[c - 'a']; }
+        public void put(char c) { children[c - 'a'] = new TrieNode(); }
+        public void setEnd() { isEnd = true; }
+        public boolean isEnd() { return isEnd; }
+    }
+
+    static class Trie {
+        private TrieNode root;
+
+        public Trie() { root = new TrieNode(); }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (!node.containsKey(c)) node.put(c);
+                node = node.get(c);
+            }
+            node.setEnd();
+        }
+
+        public boolean search(String word) {
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd();
+        }
+
+        public boolean startsWith(String prefix) {
+            return searchPrefix(prefix) != null;
+        }
+
+        private TrieNode searchPrefix(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                if (!node.containsKey(c)) return null;
+                node = node.get(c);
+            }
+            return node;
+        }
+    }
+
+    /**
+     * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。
+     *  这一数据结构有相当多的应用情景，例如自动补全和拼写检查。
+     */
+    static class TrieByHap {
+
+        private HashMap<String,String>  storeMap;
+
+        public TrieByHap() {
+            storeMap = new HashMap<>();
+        }
+
+        // 向前缀树中插入字符串 word
+        public void insert(String word) {
+            storeMap.put(word,word);
+        }
+
+        // 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+        public boolean search(String word) {
+            return storeMap.get(word) != null;
+        }
+
+        // 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+        public boolean startsWith(String prefix) {
+            boolean existWord = false;
+
+            // lambda遍历 jdk1.8后才支持
+            // storeMap.forEach((key, value) -> {
+            //     if(key.startsWith(prefix)){
+            //         System.out.println(key);
+            //     }
+            // });
+
+            // key遍历
+            for (String key : storeMap.keySet()) {
+                if(key.startsWith(prefix)){
+                    existWord = true;
+                    break;
+                }
+            }
+
+            // 迭代器遍历
+            // Iterator<Map.Entry<String, String>> it = storeMap.entrySet().iterator();
+            // while(it.hasNext()){
+            //     Map.Entry<String, String> entry = it.next();
+            //     if(entry.getKey().startsWith(prefix)){
+            //         existWord = true;
+            //         break;
+            //     }
+            //     it.next();
+            // }
+            return  existWord;
+        }
+    }
+
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
 
     static class LRUCacheByDs {
         class Node {
