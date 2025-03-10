6156a11dc8829530358027e42c482e3be6d98880
EgoistCookie <335985579@qq.com>
2025-03-10 20:33:09 +0800

    lf : AlgorithmAboutTree,add算法94.二叉树的最近公共祖先.

--- src/main/java/com/algorithm/AlgorithmAboutTree.java
+++ src/main/java/com/algorithm/AlgorithmAboutTree.java
@@ -14,21 +14,61 @@ public class AlgorithmAboutTree {
         // 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
         // root节点为层序遍历：即从上到下、从左到右的遍历方式
         // 中序遍历：先输出左节点、再输出根节点、再输出右节点
-        TreeNode rootTree1 = new TreeNode(1);
+        // TreeNode rootTree1 = new TreeNode(1);
+        // TreeNode cur = rootTree1;
+        // rootTree1.right = new TreeNode(2);
+        // cur = rootTree1.right;
+        // cur.left = new TreeNode(3);
+        // List<Integer> orderList = inOrderTraversal(rootTree1);
+        // for(int i:orderList){
+        //     System.out.println(i);
+        // }
+
+        // 算法236.二叉树的最近公共祖先
+        // 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+        // 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+        TreeNode rootTree1 = new TreeNode(3);
         TreeNode cur = rootTree1;
-        rootTree1.right = new TreeNode(2);
-        cur = rootTree1.right;
-        cur.left = new TreeNode(3);
+        rootTree1.left = new TreeNode(5);
+        rootTree1.right = new TreeNode(1);
+        rootTree1.left.left = new TreeNode(6);
+        rootTree1.left.right = new TreeNode(2);
+        rootTree1.right.left = new TreeNode(0);
+        rootTree1.right.right = new TreeNode(8);
+        rootTree1.left.right.left = new TreeNode(7);
+        rootTree1.left.right.right = new TreeNode(4);
+        rootTree1 = lowestCommonAncestor(rootTree1,new TreeNode(5),new TreeNode(1));
+        System.out.println(rootTree1.val);
 
-        TreeNode rootTree2 = new TreeNode(1);
 
-        List<Integer> orderList = inOrderTraversal(rootTree1);
-        for(int i:orderList){
-            System.out.println(i);
+    }
+
+    /**
+     *
+     * @param root [3,5,1,6,2,0,8,null,null,7,4]
+     * @param p 5
+     * @param q 1
+     * @return 3
+     */
+    static public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        TreeNode resNode = new TreeNode();
+
+        // 先找到p的所有祖先
+        List<TreeNode> tl =  equalsNode(root,p);
+        // 通过p的所有祖先，去找哪一个的子代里有q
+
+        return resNode;
+
+    }
+
+    private static List<TreeNode> equalsNode(TreeNode root, TreeNode p) {
+        List<TreeNode> tList = new ArrayList<>();
+        TreeNode curN = root;
+        while(curN.left.val != p.val){
+            curN = curN.left;
         }
-
-
-
+        return tList;
     }
 
     /**
