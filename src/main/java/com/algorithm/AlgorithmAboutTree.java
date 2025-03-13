eb41a095d6435c20312ae45f73922d808d69694c
egoistcookie <335985579@qq.com>
2025-03-13 23:51:36 +0800

    lf：AlgorithmAboutTree：算法124.二叉树中的最大路径和.解法：深度优先搜索，分别递归计算左右子树的最大路径和，计算当前节点的路径和（左子树 + 右子树 + 当前节点）（只能选择左子树或右子树中的一条路径）

--- src/main/java/com/algorithm/AlgorithmAboutTree.java
+++ src/main/java/com/algorithm/AlgorithmAboutTree.java
@@ -27,20 +27,95 @@ public class AlgorithmAboutTree {
         // 算法236.二叉树的最近公共祖先
         // 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
         // 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
-        TreeNode rootTree1 = new TreeNode(3);
-        TreeNode cur = rootTree1;
-        rootTree1.left = new TreeNode(5);
-        rootTree1.right = new TreeNode(1);
-        rootTree1.left.left = new TreeNode(6);
-        rootTree1.left.right = new TreeNode(2);
-        rootTree1.right.left = new TreeNode(0);
-        rootTree1.right.right = new TreeNode(8);
-        rootTree1.left.right.left = new TreeNode(7);
-        rootTree1.left.right.right = new TreeNode(4);
-        rootTree1 = lowestCommonAncestor(rootTree1,new TreeNode(6),new TreeNode(5));
-        System.out.println(rootTree1.val);
+        // TreeNode rootTree1 = new TreeNode(3);
+        // rootTree1.left = new TreeNode(5);
+        // rootTree1.right = new TreeNode(1);
+        // rootTree1.left.left = new TreeNode(6);
+        // rootTree1.left.right = new TreeNode(2);
+        // rootTree1.right.left = new TreeNode(0);
+        // rootTree1.right.right = new TreeNode(8);
+        // rootTree1.left.right.left = new TreeNode(7);
+        // rootTree1.left.right.right = new TreeNode(4);
+        // TreeNode cur = rootTree1;
+        // rootTree1 = lowestCommonAncestor(rootTree1,new TreeNode(6),new TreeNode(5));
+        // System.out.println(rootTree1.val);
 
+        // 算法124.二叉树中的最大路径和
+        // 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。
+        // 同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+        // 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+        // 如[1,2,3],最优路径是 2 -> 1 -> 3,返回为6
+        // [[-10,9,20,null,null,15,7]],最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+        // 解法：递归计算左右子树的最大路径和，计算当前节点的路径和（左子树 + 右子树 + 当前节点）
+        //  ,返回当前节点的最大路径和（只能选择左子树或右子树中的一条路径）
+        TreeNode rootTree1 = new TreeNode(-10);
+        rootTree1.left = new TreeNode(9);
+        rootTree1.right = new TreeNode(20);
+        rootTree1.right.left = new TreeNode(15);
+        rootTree1.right.right = new TreeNode(7);
+        int max = maxPathSum(rootTree1);
+        System.out.println(max);
 
+    }
+
+    /**
+     * 算法124.二叉树中的最大路径和
+     * @param root [-10,9,20,null,null,15,7]
+     * @return 15 + 20 + 7 = 42
+     */
+    static public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+    static private int maxSum = Integer.MIN_VALUE;
+
+    static private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        // 递归计算左右子树的最大路径和
+        int leftSum = Math.max(dfs(node.left), 0); // 如果子树路径和为负，则不取
+        int rightSum = Math.max(dfs(node.right), 0); // 如果子树路径和为负，则不取
+
+        // 计算当前节点的路径和（左子树 + 右子树 + 当前节点）
+        int currentPathSum = node.val + leftSum + rightSum;
+
+        // 更新全局最大路径和
+        maxSum = Math.max(maxSum, currentPathSum);
+
+        // 返回当前节点的最大路径和（只能选择左子树或右子树中的一条路径）
+        return node.val + Math.max(leftSum, rightSum);
+    }
+    /**
+     * 算法124.二叉树中的最大路径和
+     * @param root [-10,9,20,null,null,15,7]
+     * @return 15 + 20 + 7 = 42
+     */
+    static public int maxPathSumByMyself(TreeNode root) {
+        int total = root.val;
+        TreeNode curNode = root;
+        while(curNode.left!=null || curNode.right!=null){
+
+            int curV = curNode.val;
+            int leftV = 0;
+            if(curNode.left!=null){
+                leftV = curNode.left.val;
+            }
+            int rightV = 0;
+            if(curNode.right!=null){
+                rightV = curNode.right.val;
+            }
+            if(leftV > rightV){
+                curNode = curNode.left;
+            }else{
+                curNode = curNode.right;
+            }
+            int totalLR = leftV + rightV + curV;
+            total = Math.max(total,totalLR);
+        }
+
+        return total;
     }
 
     /**
