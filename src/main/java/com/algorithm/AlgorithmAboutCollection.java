de4beb81ac3050eb21a71996bd1d7d0df33f7f8a
EgoistCookie <335985579@qq.com>
2025-04-10 12:51:29 +0800

    lf : AlgorithmAboutCollection,add力扣74.检查二维矩阵：给你一个满足下述两条属性的 m x n 整数矩阵：给你一个整数 target ，如果 target 在矩阵中，返回 true。解法：Z字查找：从右上角开始，根据当前元素与 target 的大小关系向左或向下移动。时间复杂度：O(m+n)

--- src/main/java/com/algorithm/AlgorithmAboutCollection.java
+++ src/main/java/com/algorithm/AlgorithmAboutCollection.java
@@ -364,17 +364,39 @@ public class AlgorithmAboutCollection {
         // 39.组合总和
         // 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。
         // 解法：递归回溯+剪枝
-        List<List<Integer>> re = combinationSum(new int[]{2,3,6,7},7);
-        for(List<Integer> rel : re){
-            for(int i : rel){
-                System.out.print(i);
-            }
-            System.out.println();
-        }
+        // List<List<Integer>> re = combinationSum(new int[]{2,3,6,7},7);
+        // for(List<Integer> rel : re){
+        //     for(int i : rel){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
         // System.out.println(combinationSum(new int[]{2,3,6,7},7));
 
+        // 74.检查二维矩阵
+        // 给你一个满足下述两条属性的 m x n 整数矩阵：给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+        // 解法：Z字查找：从右上角开始，根据当前元素与 target 的大小关系向左或向下移动。时间复杂度：O(m+n)
+        System.out.println(searchMatrix(new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}},3));
 
 
+
+    }
+
+    /**
+     * 给你一个满足下述两条属性的 m x n 整数矩阵：给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+     * @param matrix {{1,3,5,7},{10,11,16,20},{23,30,34,60}}
+     * @param target 7
+     * @return true
+     */
+    static public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) col--;
+            else row++;
+        }
+        return false;
     }
 
     /**
@@ -861,30 +883,30 @@ public class AlgorithmAboutCollection {
      * @param target 5
      * @return 是否存在
      */
-    static public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
-
-        int m = matrix.length;    // 行数
-        int n = matrix[0].length; // 列数
-
-        // 从矩阵的右上角开始搜索
-        int row = 0;
-        int col = n - 1;
-
-        while (row < m && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true; // 找到目标值
-            } else if (matrix[row][col] > target) {
-                col--; // 目标值可能在当前列的左边
-            } else {
-                row++; // 目标值可能在当前行的下边
-            }
-        }
-
-        return false; // 未找到目标值
-    }
+    // static public boolean searchMatrix(int[][] matrix, int target) {
+    //     if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+    //         return false;
+    //     }
+    //
+    //     int m = matrix.length;    // 行数
+    //     int n = matrix[0].length; // 列数
+    //
+    //     // 从矩阵的右上角开始搜索
+    //     int row = 0;
+    //     int col = n - 1;
+    //
+    //     while (row < m && col >= 0) {
+    //         if (matrix[row][col] == target) {
+    //             return true; // 找到目标值
+    //         } else if (matrix[row][col] > target) {
+    //             col--; // 目标值可能在当前列的左边
+    //         } else {
+    //             row++; // 目标值可能在当前行的下边
+    //         }
+    //     }
+    //
+    //     return false; // 未找到目标值
+    // }
 
     /**
      * 两次翻转，先水平翻转，再对角线翻转，即可旋转90度
