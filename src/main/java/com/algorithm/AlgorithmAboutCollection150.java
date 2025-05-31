6cf91e99b4f5c6666cb93fb904f6e90aa6dadaea
egoistcookie <335985579@qq.com>
2025-05-31 10:48:13 +0800

    lf：AlgorithmAboutCollection150：算法88.合并两个有序数组.解法：从后往前的双指针遍历。

---
+++ src/main/java/com/algorithm/AlgorithmAboutCollection150.java
@@ -0,0 +1,39 @@
+package com.algorithm;
+
+public class AlgorithmAboutCollection150 {
+
+    public static void main(String[] args) {
+
+        AlgorithmAboutCollection150 ac = new AlgorithmAboutCollection150();
+
+        // 88.合并两个有序数组
+        // 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+        // 解法：从后往前的双指针遍历
+        ac.merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
+
+
+
+
+    }
+
+
+    /**
+     * 合并两个有序数组
+     * @param nums1 1,2,3,0,0,0
+     * @param m 3
+     * @param nums2 2,5,6
+     * @param n 3
+     *          1,2,2,3,5,6
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int cur1 = m - 1, cur2 = n - 1, i = m + n - 1;
+        while (cur1 >= 0 && cur2 >= 0) {
+            nums1[i--] = (nums1[cur1] > nums2[cur2]) ? nums1[cur1--] : nums2[cur2--];
+        }
+        while (cur2 >= 0) {
+            nums1[i--] = nums2[cur2--];
+        }
+    }
+
+
+}
