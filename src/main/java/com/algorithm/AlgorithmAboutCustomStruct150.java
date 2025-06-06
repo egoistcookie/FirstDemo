aee71538fa0751bcd875ec94b2229b453b8b8779
EgoistCookie <335985579@qq.com>
2025-06-06 12:42:19 +0800

    lf : AlgorithmAboutCollection150，add算法380.实现插入删除和查询随机元素的复杂度都为O1的数据结构.解法：hashmap+arraylist.

---
+++ src/main/java/com/algorithm/AlgorithmAboutCustomStruct150.java
@@ -0,0 +1,70 @@
+package com.algorithm;
+
+import java.util.*;
+
+/**
+ * 自定义数据结构的算法类
+ */
+public class AlgorithmAboutCustomStruct150 {
+
+
+
+    public static void main(String[] args) {
+
+
+        AlgorithmAboutCustomStruct150 ac = new AlgorithmAboutCustomStruct150();
+
+        // 380.实现插入删除和查询随机元素的复杂度都为O1的数据结构
+        // 解法：hashmap+arraylist
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1)); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+        System.out.println(randomizedSet.remove(2)); // 返回 false ，表示集合中不存在 2 。
+        System.out.println(randomizedSet.insert(2)); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+        System.out.println(randomizedSet.getRandom()); // getRandom 应随机返回 1 或 2 。
+        System.out.println(randomizedSet.remove(1)); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+        System.out.println(randomizedSet.insert(2)); // 2 已在集合中，所以返回 false 。
+        System.out.println(randomizedSet.getRandom()); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+
+
+    }
+
+
+    static class RandomizedSet {
+        private List<Integer> list;
+        private Map<Integer, Integer> map;
+        private Random random;
+
+        public RandomizedSet() {
+            list = new ArrayList<>();
+            map = new HashMap<>();
+            random = new Random();
+        }
+
+        public boolean insert(int val) {
+            if (map.containsKey(val)) {
+                return false;
+            }
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+
+        public boolean remove(int val) {
+            if (!map.containsKey(val)) {
+                return false;
+            }
+            int index = map.get(val);
+            int lastElement = list.get(list.size() - 1);
+            list.set(index, lastElement);
+            map.put(lastElement, index);
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+        }
+
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+
+}
