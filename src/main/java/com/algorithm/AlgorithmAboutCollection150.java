b7ea74476b9b229ce1cf2dde9da88859de89ccdc
egoistcookie <335985579@qq.com>
2025-06-14 12:30:33 +0800

    lf：AlgorithmAboutCollection150：算法28.找出字符串中第一个匹配项的下标。给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。解法：indexOf。

--- src/main/java/com/algorithm/AlgorithmAboutCollection150.java
+++ src/main/java/com/algorithm/AlgorithmAboutCollection150.java
@@ -90,14 +90,31 @@ public class AlgorithmAboutCollection150 {
         // 151.反转字符串中的单词
         // 给你一个字符串 s ，请你反转字符串中 单词 的顺序。
         // 解法：遍历+栈存单词。或者双指针反转。
-        System.out.println(ac.reverseWords("the sky is blue"));
-        System.out.println(ac.reverseWords("  hello world  "));
-        System.out.println(ac.reverseWords("a good   example"));
+        // System.out.println(ac.reverseWords("the sky is blue"));
+        // System.out.println(ac.reverseWords("  hello world  "));
+        // System.out.println(ac.reverseWords("a good   example"));
+
+        // 28.找出字符串中第一个匹配项的下标
+        // 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+        // 如果 needle 不是 haystack 的一部分，则返回  -1 。
+        // 解法：indexOf
+        System.out.println(ac.strStr("sadbutsad","sad"));
+        System.out.println(ac.strStr("leetcode","leeto"));
 
 
 
     }
 
+
+    /**
+     * 28.找出字符串中第一个匹配项的下标
+     * @param haystack "sadbutsad"
+     * @param needle "sad"
+     * @return 0
+     */
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
 
     /**
      * 151.反转字符串中的单词
